cfb75be485a601d89f3b5c0c28ef9e61b548811b
Bruna Belini <brunabbelini1@hotmail.com>
2025-03-27 12:38:11 -0300

    Signal "clicked" de cada botão e permitindo apenas números válidos no display ao pressionar botões

--- buttons.py
+++ buttons.py
@@ -1,6 +1,9 @@
+from display import Display
+from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QGridLayout, QPushButton
-from util import isEmpty, isNumOrDot
+from util import isEmpty, isNumOrDot, isValidNumber
 from variables import MEDIUM_FONT_SIZE
+
 
 class Button(QPushButton): 
     def __init__(self, *args, **kwargs):
@@ -13,8 +16,9 @@ class Button(QPushButton):
         self.setFont(font)
         self.setMinimumSize(75, 75)
 
+
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -24,7 +28,7 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
-
+        self.display = display
         self._makeGrid()
 
     def _makeGrid(self):
@@ -32,9 +36,52 @@ class ButtonsGrid(QGridLayout):
             for colNumber, buttonText in enumerate(rowData):
                 button = Button(buttonText)
 
-                # if buttonText not in '0123456789.':
-                #     button.setProperty('cssClass', 'specialButton')
                 if not isNumOrDot(buttonText) and not isEmpty(buttonText):
                     button.setProperty('cssClass', 'specialButton')
 
                 self.addWidget(button, rowNumber, colNumber)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+                button.clicked.connect(buttonSlot) # type: ignore
+
+    # def _makeGrid(self):
+    #     for i, row in enumerate(self._gridMask):
+    #         for j, buttonText in enumerate(row):
+    #             if buttonText == '':
+    #                 continue
+                
+    #             if buttonText == '0':
+    #                 button = Button(buttonText)
+    #                 # self.addWidget(button, i, j, 1, 2)
+    #                 self.addWidget(button, 4, 0, 1, 2)
+    #                 # j += 1
+    #             else: 
+    #                 button = Button(buttonText)
+    #                 if not isNumOrDot(buttonText) and not isEmpty(buttonText):
+    #                     button.setProperty('cssClass', 'specialButton')
+
+    #                 self.addWidget(button, i, j)
+    #                 buttonSlot = self._makeButtonDisplaySlot(
+    #                     self._insertButtonTextToDisplay,
+    #                     button,
+    #                 )
+    #                 button.clicked.connect(buttonSlot) # type: ignore
+                    
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button):
+        buttonText = button.text()
+        newDisplayValue = self.display.text() + buttonText
+
+        if not isValidNumber(newDisplayValue):
+            return
+
+        self.display.insert(buttonText)
+        
--- util.py
+++ util.py
@@ -4,9 +4,18 @@ import re
 NUM_OR_DOT_REGEX = re.compile(r'^[0-9.]$') # começa com e termina com
 
 
-
 def isNumOrDot(string: str):
     return bool(NUM_OR_DOT_REGEX.search(string))
+
+
+def isValidNumber(string: str):
+    valid = False
+    try:
+        float(string)
+        valid = True
+    except ValueError:
+        valid = False
+    return valid
 
 
 def isEmpty(string: str):
